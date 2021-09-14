151f9c1bb67146e032732a1539f6c87eb672da16
asdru333 <89029869+asdru333@users.noreply.github.com>
2021-09-14 10:02:22 -0600

    Update Actividad.h

--- Actividad.h
+++ Actividad.h
@@ -7,73 +7,73 @@
 class Actividad
 {
 	protected:
-    Actividad* padre;
-    //Tipo* tipos
-    std::string responsable; 
+		Actividad* padre;
+		//Tipo* tipos
+		std::string responsable; 
 		std::string fechaPlanteadaInicio;
 		std::string fechaPlanteadaFinal;
 		std::string fechaRealInicio;
 		std::string fechaRealFinalizacion;
-    std::string descripcion;
+		std::string descripcion;
 		
 	public: 
 		Actividad* getPadre()
-    {
-      return this->padre;
-    }
- 	void setPadre(Actividad* nuevoPadre)
- 	{
-		this->padre = nuevoPadre;
-	}
-    std::string getResponsable()
-    {
-      	return this->responsable;
-    }
-    void setResponsable(std::string nuevoResponsable)
-    {
-    	this->responsable = nuevoResponsable;
-    }
-	std::string getFechaPlanteadaInicio()
-	{
-		return this->fechaPlanteadaInicio;
-	}
-	void setFechaPlanteadaInicio(std::string NuevafechaPlanteadaInicio)
-	{
-		this->fechaPlanteadaInicio = NuevafechaPlanteadaInicio;
-	}
-	std::string getFechaPlanteadaFinal()
-	{
-		return this->fechaPlanteadaFinal;
-	}
-	void setFechaPlanteadaFinal(std::string nuevaFechaPlanteadaFinal)
-	{
-		this->fechaPlanteadaFinal = nuevaFechaPlanteadaFinal;
-	}
-	std::string getFechaRealInicio()
-	{
-		return this->fechaRealInicio;
-	}
-	void setFechaRealInicio(std::string nuevaFechaRealFinal)
-	{
-		this->fechaRealInicio = nuevaFechaRealFinal;
-	}
-	std::string getFechaRealFinalizacion()
-	{
-		return this->fechaRealFinalizacion;
-	}
-	void setFechaRealFinalizacion(std::string nuevaFechaRealFinalizacion)
-	{
-		this->fechaRealFinalizacion = nuevaFechaRealFinalizacion;
-	}
-    std::string getDescripcion()
-	{
-		return this->descripcion;
-	}
-	void setDescripcion(std::string nuevaDescripcion)
-	{
-		this->descripcion = nuevaDescripcion;
-	}
-    virtual void calcularFechas() = 0;
-    virtual void add(Actividad*) = 0;
-    virtual void remove(Actividad*) = 0;
+		{
+			return this->padre;
+		}
+		void setPadre(Actividad* nuevoPadre)
+		{
+			this->padre = nuevoPadre;
+		}
+		std::string getResponsable()
+		{
+			return this->responsable;
+		}
+		void setResponsable(std::string nuevoResponsable)
+		{
+			this->responsable = nuevoResponsable;
+		}
+		std::string getFechaPlanteadaInicio()
+		{
+			return this->fechaPlanteadaInicio;
+		}
+		void setFechaPlanteadaInicio(std::string NuevafechaPlanteadaInicio)
+		{
+			this->fechaPlanteadaInicio = NuevafechaPlanteadaInicio;
+		}
+		std::string getFechaPlanteadaFinal()
+		{
+			return this->fechaPlanteadaFinal;
+		}
+		void setFechaPlanteadaFinal(std::string nuevaFechaPlanteadaFinal)
+		{
+			this->fechaPlanteadaFinal = nuevaFechaPlanteadaFinal;
+		}
+		std::string getFechaRealInicio()
+		{
+			return this->fechaRealInicio;
+		}
+		void setFechaRealInicio(std::string nuevaFechaRealFinal)
+		{
+			this->fechaRealInicio = nuevaFechaRealFinal;
+		}
+		std::string getFechaRealFinalizacion()
+		{
+			return this->fechaRealFinalizacion;
+		}
+		void setFechaRealFinalizacion(std::string nuevaFechaRealFinalizacion)
+		{
+			this->fechaRealFinalizacion = nuevaFechaRealFinalizacion;
+		}
+		std::string getDescripcion()
+		{
+			return this->descripcion;
+		}
+		void setDescripcion(std::string nuevaDescripcion)
+		{
+			this->descripcion = nuevaDescripcion;
+		}
+		virtual void calcularFechas() = 0;
+		virtual void add(Actividad*) = 0;
+		virtual void remove(Actividad*) = 0;
 };
