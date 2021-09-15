e172cf1bba8b7b02fd1458e61e3d0bffca5d85ba
asdru333 <89029869+asdru333@users.noreply.github.com>
2021-09-15 13:14:52 -0600

    Create ActividadSimple.hpp

---
+++ ActividadSimple.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <string>
+#include "Actividad.hpp"
+
+class ActividadSimple : public Actividad
+{	
+	public: 
+		Actividad(std::string responsable, std::string fechaPlanteadaInicio, std::string fechaPlanteadaFinal, 
+              	std::string fechaRealInicio, std::string fechaRealFinalizacion, std::string descripcion, Actividad* padre)
+		{ 
+			this->responsable = responsable;
+			this->fechaPlanteadaInicio = fechaPlanteadaInicio;
+			this->fechaPlanteadaFinal = fechaPlanteadaFinal;
+			this->fechaRealInicio = fechaRealInicio;
+			this->fechaRealFinalizacion = fechaRealFinalizacion;
+			this->descripcion = descripcion;
+			this->padre = padre;
+		}
+		
+		~Actividad()
+		{
+			this->padre = nullptr;
+		}
+		
+		virtual void calcularFechas() override
+		{
+		    ;
+		}
+		virtual void add(Actividad*) override
+		{
+		    ;
+		}
+		virtual void remove(Actividad*) override
+		{
+		    ;
+		}
+};
