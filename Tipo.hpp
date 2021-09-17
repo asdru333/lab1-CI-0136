d3d59994b866125774414d071b4e4e1a06e7b72a
asdru333 <89029869+asdru333@users.noreply.github.com>
2021-09-17 11:25:11 -0600

    Create Tipo.hpp

---
+++ Tipo.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <string>
+
+class Tipo
+{
+	private:
+		std::string nombre;
+		Tipo* padre;
+		Tipo* hijo;
+			
+	public: 
+		Tipo(std::string nombre, Tipo* padre = nullptr, Tipo* hijo = nullptr)
+		{
+			this->nombre = nombre;
+			this->padre = padre;
+			this->hijo = hijo;
+		}
+	
+		std::string getNombre()
+		{
+			return this->nombre;
+		}
+		void setNombre(std::string nuevoNombre)
+		{
+			this->nombre = nuevoNombre;
+		}
+		
+		Tipo* getPadre()
+		{
+			return this->padre;
+		}
+		void setPadre(Tipo* nuevoPadre)
+		{
+			this->padre = nuevoPadre;
+		}
+	
+		Tipo* getHijo()
+		{
+			return this->padre;
+		}
+		void setHijo(Tipo* nuevoHijo)
+		{
+			this->padre = nuevoHijo;
+		}
+};
