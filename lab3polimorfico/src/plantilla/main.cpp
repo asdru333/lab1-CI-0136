2711f77ab1415583616b98f6b02fe305c366da22
asdru333 <89029869+asdru333@users.noreply.github.com>
2021-09-30 12:34:37 -0600

    Update main.cpp

--- lab3polimorfico/src/plantilla/main.cpp
+++ lab3polimorfico/src/plantilla/main.cpp
@@ -9,32 +9,32 @@
 int main()
 {
 	std::string hola("hola");
-    Compresor1<std::stringstream> compresorSs;
-    compresorSs << hola;
-    std::cout << compresorSs.str();
+	Compresor1<std::stringstream> compresorSs;
+    	compresorSs << hola;
+	std::cout << compresorSs.str();
     
-    std::cout << std::endl;
+    	std::cout << std::endl;
     
-    Compresor1<std::ostringstream> compresorOs;
-    compresorOs << hola;
-    std::cout << compresorOs.str();
+	Compresor1<std::ostringstream> compresorOs;
+	compresorOs << hola;
+	std::cout << compresorOs.str();
     
-    std::cout << std::endl;
+	std::cout << std::endl;
     
-    Compresor1<std::fstream> compresorFs;
-  	compresorFs.open ("text1fs.txt", std::fstream::out);
-    compresorFs << hola;
-    compresorFs.close();
+	Compresor1<std::fstream> compresorFs;
+	compresorFs.open ("text1fs.txt", std::fstream::out);
+	compresorFs << hola;
+	compresorFs.close();
     
-    Compresor1<std::ofstream> compresorOfs;
+	Compresor1<std::ofstream> compresorOfs;
   	compresorOfs.open ("text1ofs.txt", std::fstream::out);
-    compresorOfs << hola;
-    compresorOfs.close();
+	compresorOfs << hola;
+	compresorOfs.close();
     
-    Compresor2<std::stringstream> compresorSs2;
-    compresorSs2 << hola;
-    std::cout << compresorSs2.str();
+	Compresor2<std::stringstream> compresorSs2;
+	compresorSs2 << hola;
+	std::cout << compresorSs2.str();
     
-    return 0;
+	return 0;
 }
 
