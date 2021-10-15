458b3d19d7da347debb9ce62067d2ebf991fa76f
asdru333 <89029869+asdru333@users.noreply.github.com>
2021-10-15 17:53:00 -0600

    Update main.cpp

--- lab4/src/main.cpp
+++ lab4/src/main.cpp
@@ -1,21 +1,44 @@
 #include <iostream>
+#include <string>
+#include <stack>
 
-#include "OperandoDouble.hpp"
+bool verificarSigno(std::string str)
+{
+    if (str.compare("+") == 0 or str.compare("-") == 0 or str.compare("*") == 0 or str.compare("/") == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
 
-int main() {
-  OperandoDouble op1(1024.5);
-  OperandoDouble op2("765.2");
-  OperandoDouble res = op1 + op2;
-  std::cout << "Suma: ";
-  std::cout << res.getValor() << std::endl;
-  res = op1 - op2;
-  std::cout << "Resta: ";
-  std::cout << res.getValor() << std::endl;
-  res = op1 * op2;
-  std::cout << "Multiplicacion: ";
-  std::cout << res.getValor() << std::endl;
-  res = op1 / op2;
-  std::cout << "Division: ";
-  std::cout << res.getValor() << std::endl;
-  return 0;
+void split(std::string s, std::string del = " ")
+{
+    std::stack<std::string> operandos;
+    int start = 0;
+    int end = s.find(del);
+    std::string substr;
+    while (end != -1) 
+    {
+        substr = s.substr(start, end - start);
+        if (substr.size() == 1 and verificarSigno(substr))
+        {
+            std::cout << substr << std::endl;
+            //operar()
+        }
+        else
+        {
+            operandos.push(s.substr(start, end - start));
+        }
+        start = end + del.size();
+        end = s.find(del, start);
+    }
+    std::cout << s.substr(start, end - start) << std::endl;
+    //operar()
+}
+
+int main()
+{
+    std::string a = "3.5 2.8 * 9.5 -";
+    split(a);
+    return 0;
 }
