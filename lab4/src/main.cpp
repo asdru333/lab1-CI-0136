0c9d9d990c768a713228941ecb138b2fbd65933c
asdru333 <89029869+asdru333@users.noreply.github.com>
2021-10-15 18:28:13 -0600

    Update main.cpp

--- lab4/src/main.cpp
+++ lab4/src/main.cpp
@@ -11,34 +11,77 @@ bool verificarSigno(std::string str)
     return 0;
 }
 
-void split(std::string s, std::string del = " ")
+void operar(std::stack<std::string> &operandos, std::string tipo, std::string signo)
+{
+    std::string operando1 = operandos.top();
+    operandos.pop();
+    std::string operando2 = operandos.top();
+    operandos.pop();
+    std::string resultado;
+    
+    //if tipo (aqui se determina el operando)
+    
+    switch (signo[0]) // cambiar los cases para que se ajuste a lo que se necesita
+    {
+        case '+':
+            std::cout << operando1 << " + " << operando2 << std::endl;
+            resultado = "1.1";
+            operandos.push(resultado);
+            break;
+            
+        case '-':
+            std::cout << operando1 << " - " << operando2 << std::endl;
+            resultado = "1.2";
+            operandos.push(resultado);
+            break;
+            
+        case '*':
+            std::cout << operando1 << " * " << operando2 << std::endl;
+            resultado = "1.3";
+            operandos.push(resultado);
+            break;
+            
+        case '/':
+            std::cout << operando1 << " / " << operando2 << std::endl;
+            resultado = "1.4";
+            operandos.push(resultado);
+            break;
+            
+        default:
+            std::cout << "simbolo invalido" << std::endl;
+            break;
+    }
+}
+
+void split(std::string operacion, std::string tipo, std::string del = " ")
 {
     std::stack<std::string> operandos;
     int start = 0;
-    int end = s.find(del);
+    int end = operacion.find(del);
     std::string substr;
     while (end != -1) 
     {
-        substr = s.substr(start, end - start);
+        substr = operacion.substr(start, end - start);
         if (substr.size() == 1 and verificarSigno(substr))
         {
-            std::cout << substr << std::endl;
-            //operar()
+            operar(operandos, tipo, substr);
         }
         else
         {
-            operandos.push(s.substr(start, end - start));
+            operandos.push(operacion.substr(start, end - start));
         }
         start = end + del.size();
-        end = s.find(del, start);
+        end = operacion.find(del, start);
     }
-    std::cout << s.substr(start, end - start) << std::endl;
-    //operar()
+    substr = operacion.substr(start, end - start);
+    operar(operandos, tipo, substr);
 }
 
 int main()
 {
     std::string a = "3.5 2.8 * 9.5 -";
-    split(a);
+    std::string b = "entero";
+    split(a, b);
+    std::cout << "listo";
     return 0;
 }
